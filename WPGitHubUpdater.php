f9bb9c3913417e353625ebd783fae5cd3f79f3f9
Martin Jensen <49763446+graybirchsolutions@users.noreply.github.com>
2021-02-18 15:30:23 -0500

    Rename file and class to conform to PSR-4

--- WPGitHubUpdater.php
+++ WPGitHubUpdater.php
@@ -33,7 +33,8 @@ if ( ! defined( 'ABSPATH' ) || class_exists( 'WPGitHubUpdater' ) || class_exists
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-class WP_GitHub_Updater {
+// Class name changed to eliminate underscore to comply with PSR-4/PSR-0 autoload
+class WPGitHubUpdater {
 
 	/**
 	 * GitHub Updater version
